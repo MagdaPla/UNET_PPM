210d57cc02b4a36d544bc4ee37b2783d564b310d
MagdaPla <magdaplamontferrer@gmail.com>
2020-06-03 00:07:46 +0200

    modificacions_conversions format

--- Conversions_format.R
+++ Conversions_format.R
@@ -1,23 +1,20 @@
-# per probar el codi senzill de carvana canviem els formats originals a jpg i gif
+# per probar el codi senzill de carvana canviem els formats originals que hem generat en TIFF a jpg i gif
 
-# llegim els tif
 
 library(magick)
 library(tiff)
 
-setwd("C:/Taller/UOC/Aules/TFM/Exemples/UNET_PPM")
-
 #convertim mascares 
 
 for (tile in 1:144){
-  mascara <- image_read(paste("C:/Taller/UOC/Aules/TFM/Exemples/UNET_PPM/masc/",tile,".tif", sep = ""))
-  image_write(mascara, path=(paste("C:/Taller/UOC/Aules/TFM/Exemples/UNET_PPM/masc_gif/",tile,".gif",sep = "")), format = "gif")
+  mascara <- image_read(paste("C:/Taller/UOC/Aules/TFM/Naves/rgb/masc/",tile,".tif", sep = ""))
+  image_write(mascara, path=(paste("C:/Taller/UOC/Aules/TFM/Naves/rgb/masc0_gif/",tile,".gif",sep = "")), format = "gif")
 }
 
 
 #convertim rgb 
 
 for (tile in 1:144){
-  rgb <- image_read(paste("C:/Taller/UOC/Aules/TFM/Exemples/UNET_PPM/rgb/",tile,".tif", sep = ""))
-  image_write(rgb, path=(paste("C:/Taller/UOC/Aules/TFM/Exemples/UNET_PPM/rgb_jpg/",tile,".jpg",sep = "")), format = "jpg")
+  rgb <- image_read(paste("C:/Taller/UOC/Aules/TFM/Naves/rgb/rgb/",tile,".tif", sep = ""))
+  image_write(rgb, path=(paste("C:/Taller/UOC/Aules/TFM/Naves/rgb/rgb0_jpg/",tile,".jpg",sep = "")), format = "jpg")
 }
